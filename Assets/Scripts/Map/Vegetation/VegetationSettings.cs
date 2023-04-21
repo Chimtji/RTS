6fc54d67ec7077c8ef1c6dff90ea1ddc7d706383
MANFLU <kim@orsoe.me>
2023-04-21 10:02:57 +0200

    test

---
+++ Assets/Scripts/Map/Vegetation/VegetationSettings.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class VegetationSettings : HeightMapSettings
+{
+    [Header("Trees")]
+    public GameObject treePrefab;
+    [Range(0, 10)]
+    public float treeMaxHeight;
+    [Range(0, 10)]
+    public float treeMinHeight;
+    public float treeNoiseMax;
+    public float treeNoiseMin;
+
+#if UNITY_EDITOR
+    protected override void OnValidate()
+    {
+        noiseSettings.validateValues();
+        base.OnValidate();
+    }
+
+#endif
+}
--- Assets/Scripts/Utils/Utils.cs
+++ Assets/Scripts/Utils/Utils.cs
@@ -6,36 +6,14 @@ namespace Trout.Utils
 {
     public class Utils
     {
-        public const int sortingOrderDefault = 5000;
-
-        public static Vector3 GetMouseWorldPosition()
+        public static void ClearChildren(Transform container)
         {
-            Vector3 mousePos = Input.mousePosition;
-            mousePos.z = -Camera.main.transform.position.z;
-            Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
-            return worldPos;
+            int children = container.transform.childCount;
+            for (int i = children - 1; i >= 0; i--)
+            {
+                GameObject.DestroyImmediate(container.transform.GetChild(i).gameObject);
+            }
         }
 
-        public static TextMesh CreateWorldText(string text, Transform parent = null, Vector3 localPosition = default(Vector3), int fontSize = 40, Color? color = null, TextAnchor textAnchor = TextAnchor.UpperLeft, TextAlignment textAlignment = TextAlignment.Left, int sortingOrder = sortingOrderDefault)
-        {
-            if (color == null) color = Color.white;
-            return CreateWorldText(parent, text, localPosition, fontSize, (Color)color, textAnchor, textAlignment, sortingOrder);
-        }
-
-        public static TextMesh CreateWorldText(Transform parent, string text, Vector3 localPosition, int fontSize, Color color, TextAnchor textAnchor, TextAlignment textAlignment, int sortingOrder)
-        {
-            GameObject gameObject = new GameObject("World_Text", typeof(TextMesh));
-            Transform transform = gameObject.transform;
-            transform.SetParent(parent, false);
-            transform.localPosition = localPosition;
-            TextMesh textMesh = gameObject.GetComponent<TextMesh>();
-            textMesh.anchor = textAnchor;
-            textMesh.alignment = textAlignment;
-            textMesh.text = text;
-            textMesh.fontSize = fontSize;
-            textMesh.color = color;
-            textMesh.GetComponent<MeshRenderer>().sortingOrder = sortingOrder;
-            return textMesh;
-        }
     }
 }
